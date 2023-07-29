8cedd677813f5cb729a8194d00bc3034e3547efb
jayasuriya1 <jaya1999suriya1@gmail.com>
2023-07-30 00:18:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter,Route, Routes,Navigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Component/login';
+import Dashboard from './Component/dashBoard';
+import CreateUrl from './Component/createUrl';
+import UrlList from './Component/urlList';
+import UrlRedirect from './Component/urlRedirect';
+import NoPage from './Component/noPage';
+import SignUp from './Component/signUp';
+import ForgotPassword from './Component/forgotPassword';
+import ResetPassword from './Component/resetPassword';
+import EmailVerification from './Component/emailVerification';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/login"/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<SignUp/>}/>
+        <Route path="/create" element={<CreateUrl/>}/>
+        <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+        <Route path="/resetPassword/:id/:token" element={<ResetPassword/>}/>
+        <Route  path="/email/verification/:id" element={<EmailVerification/>}/>
+        <Route path="/link/details" element={<UrlList/>}/>
+        <Route path="/:shortUrl" element={<UrlRedirect/>}/>
+        <Route path="/nopage" element={<NoPage/>}/>
+        <Route path="*" element={<NoPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
